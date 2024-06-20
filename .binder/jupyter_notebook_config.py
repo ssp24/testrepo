cf1126dd035290f5dddb058f17177e30c8673367
ssp24 <62755563+ssp24@users.noreply.github.com>
2024-06-20 11:10:05 +0200

    Test4

--- .binder/jupyter_notebook_config.py
+++ .binder/jupyter_notebook_config.py
@@ -1,1 +1,3 @@
-c.JupyterAISettings.default_provider = "openai:gpt-3.5-turbo"
+#c.JupyterAISettings.default_provider = "openai:gpt-3.5-turbo"
+c.ServerApp.file_to_run = 'print("Dies ist eine Testnachricht!")'
+c.LabApp.default_url = '/lab/tree/testbook.ipynb'
