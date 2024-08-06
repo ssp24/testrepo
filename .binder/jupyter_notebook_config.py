151b3b4883bd1de267310d7d2f256b74fd2d0c08
ssp24 <62755563+ssp24@users.noreply.github.com>
2024-08-06 12:39:49 +0200

    Update jupyter_notebook_config.py

--- .binder/jupyter_notebook_config.py
+++ .binder/jupyter_notebook_config.py
@@ -1,6 +1,15 @@
 # Traitlet configuration file for jupyter-notebook.
 
 c.ServerProxy.servers = {
+    'openrefine': {
+        'command': ['echo', '-p', '{port}', '-d', 'placeholder', '-i', '0.0.0.0'],
+        'port': 3333,
+        'timeout': 60,
+        'launcher_entry': {
+            'enabled': True,
+            'title': 'OpenRefine Session',
+        },
+    },
     'welcome': {
         'command': ['echo', 'This is a placeholder command'],
         'absolute_url': False,
