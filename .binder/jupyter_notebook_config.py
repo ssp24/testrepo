b9e9f141f8c9785f09ccf8b1d378fa5d43480c1b
ssp24 <62755563+ssp24@users.noreply.github.com>
2024-08-06 10:28:42 +0200

    Update jupyter_notebook_config.py

--- .binder/jupyter_notebook_config.py
+++ .binder/jupyter_notebook_config.py
@@ -1,13 +1,6 @@
 # Traitlet configuration file for jupyter-notebook.
 
-c.ServerProxy.servers = {
-    'openrefine': {
-        'command': ['/home/jovyan/.openrefine/refine', '-p', '{port}', '-d', '/home/jovyan/openrefine', '-i', '0.0.0.0'],
-        'port': 3333,
-        'timeout': 60,
-        'launcher_entry': {
-            'enabled': True,
-            'title': 'Test Traitlet',
-        },
-    },
-}
+c.LabApp.default_url = '/lab/tree/welcome.md'
+
+c.ServerApp.contents_manager_class = "jupyter_server.services.contents.largefilemanager.LargeFileManager"
+
